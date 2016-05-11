130b4e413a9eff052796a105a6c3db99a39c4b78
minoieka <minoieka@gmail.com>
2016-05-12 05:03:23 +0800

    staff

---
+++ db/migrate/20160511232218_create_staffs.rb
@@ -0,0 +1,13 @@
+class CreateStaffs < ActiveRecord::Migration
+  def change
+    create_table :staffs do |t|
+
+    	t.string :staff_id
+    	t.string :user_id
+    	t.string :staff_position
+    	t.string :staff_email
+
+      t.timestamps null: false
+    end
+  end
+end
